e2c9ca1f303353ec0efe4d06b1e2e702d540e390
lky <597572246@qq.com>
2023-06-15 09:40:46 +0000

    fix: set global flag to stop

--- src/sig.c
+++ src/sig.c
@@ -1,14 +1,19 @@
+#include "ds.h"
 #include "worker.h"
+#include "sock.h"
 #include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+
+int stop_all = 0;
 
 void sig_handler(int signo) {
-    destory_workers();
-    exit(EXIT_SUCCESS);
+    stop_all = 1;
 }
 
 int set_sighandler() {
+    printf(PR_BLUE "Setting signal handler...\n" PR_END);
     if (signal(SIGINT, sig_handler) == SIG_ERR) {
         perror("Set SIGINT handler");
         return -1;
